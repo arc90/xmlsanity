388c36cc1674aa39bcecfe054c786a025a358e0d
avi@localhost <avi@localhost>
2009-08-13 22:50:04 -0400

    added a pool for validators for better performance

---
+++ src/com/arc90/xml/validation/ValidatorPool.java
@@ -0,0 +1,35 @@
+package com.arc90.xml.validation;
+
+import javax.xml.validation.Schema;
+import javax.xml.validation.Validator;
+
+import com.arc90.util.Pool;
+
+public class ValidatorPool extends Pool<javax.xml.validation.Validator>
+{
+	private final Schema schema;
+	
+	public ValidatorPool(Schema schema)
+	{
+		this.schema = schema;
+	}
+
+	@Override
+	protected Validator create()
+	{
+		return schema.newValidator();
+	}	
+	
+	@Override
+	public void expire(Validator o)
+	{		
+	}
+
+	@Override
+	public boolean validate(Validator o)
+	{
+		return true;
+	}
+
+
+}
