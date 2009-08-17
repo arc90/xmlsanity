7d8ed7aaeead1c19be39c44a88bb490de7e5c3b5
avi <devnull@localhost>
2009-08-17 18:10:49 -0400

    tweak

--- src/com/arc90/xmlsanity/transformation/TransformationResult.java
+++ src/com/arc90/xmlsanity/transformation/TransformationResult.java
@@ -1,21 +1,27 @@
 package com.arc90.xmlsanity.transformation;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.io.StringWriter;
 
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.stream.StreamResult;
 
+import org.jdom.Document;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
 public class TransformationResult
 {
 
-    protected TransformerException error = null;
-    protected final StreamResult streamResult = new StreamResult(new StringWriter());
+    protected TransformerException error        = null;
+    protected final StreamResult   streamResult = new StreamResult(new StringWriter());
 
     public boolean errorExists()
     {
         return error != null;
     }
-    
+
     public TransformerException getError()
     {
         return error;
@@ -41,6 +47,23 @@ public class TransformationResult
         {
             return "";
         }
+    }
+
+    /**
+     * 
+     * @return If output exists, a JDOM Document of that output. Otherwise, an empty Document.
+     * @throws JDOMException
+     * @throws IOException
+     */
+    public Document toDocument() throws JDOMException, IOException
+    {
+        if (!outputExists())
+        {
+            // TODO: maybe this should return null?
+            return new Document();
+        }
+
+        return new SAXBuilder().build(new StringReader(this.toString()));
     }
 
     protected StreamResult getStreamResult()
